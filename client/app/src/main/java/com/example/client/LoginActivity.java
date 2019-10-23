a9f5dd73a53b7dc30cbcf8a5bff6da9cdd274291
Heeyeon Lee <heeyoenl@byu.edu>
2019-10-23 14:00:42 -0600

    started new

---
+++ client/app/src/main/java/com/example/client/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.example.client;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button loginButton = (Button)findViewById(R.id.login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
